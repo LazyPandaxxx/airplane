7d2d59c76b9c926a57101ce97c1eef2d80c309fe
Muharrir <muharrir.@MacBook-Air.local>
2022-05-28 21:30:33 +0700

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/splash_page.dart';
 // import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -9,9 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GetStartedPage(),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/get-started': (context) => const GetStartedPage(),
+      },
     );
   }
 }
--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,8 +1,23 @@
+import 'dart:async';
+
 import 'package:airplane/shared/theme.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushNamed(context, '/get-started');
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
