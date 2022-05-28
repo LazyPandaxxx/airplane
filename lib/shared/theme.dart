3c8e8f24c65b1bbf2d6cf5d76f0328428b35b838
Muharrir <muharrir.@MacBook-Air.local>
2022-05-28 21:35:12 +0700

    add default margin & radius

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 24.0;
+double defaultRadius = 17.0;
+
 // NOTE: COLOR
 Color kPrimaryColor = const Color(0xff5C40CC);
 Color kBlackColor = const Color(0xff1F1449);
-Color kWhiteColor= const Color(0xffFFFFFF);
+Color kWhiteColor = const Color(0xffFFFFFF);
 Color kGreyColor = const Color(0xff9698A9);
 Color kGreenColor = const Color(0xff0EC3AE);
 Color kRedColor = const Color(0xffEB70A5);
 Color kBackgroundColor = const Color(0xffFAFAFA);
 Color kInactiveColor = const Color(0xffDBD7EC);
-
 
 // NOTE: TEXTSTYLE
 TextStyle blackTextStyle = GoogleFonts.poppins(
--- lib/ui/pages/get_started_page.dart
+++ lib/ui/pages/get_started_page.dart
@@ -54,7 +54,7 @@ class GetStartedPage extends StatelessWidget {
                     style: TextButton.styleFrom(
                       backgroundColor: kPrimaryColor,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(17),
+                        borderRadius: BorderRadius.circular(defaultRadius),
                       ),
                     ),
                     child: Text(
